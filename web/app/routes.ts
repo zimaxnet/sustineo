00107b7e2d2e4c25bfaf049275b0a70198cab7d6
Burke Holland <burkeholland@gmail.com>
2025-05-06 13:32:32 -0500

    add landing page

--- web/app/routes.ts
+++ web/app/routes.ts
@@ -2,4 +2,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
+  route("/landing", "routes/landing.tsx"),
   route("/favicon.svg", "favicon.tsx")] satisfies RouteConfig;
